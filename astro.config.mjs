bff34b8975bf8c2dfbc19c8bf26e79b33e8acc1a
Netanel Gilad <netanelg@wix.com>
2025-06-06 08:35:53 +0300

    member profile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import wix from "@wix/astro";
-
+import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
@@ -9,11 +9,12 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: wix(),
+
+  adapter: cloudflare(),
 
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(),wix(),]
 });
