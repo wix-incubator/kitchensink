700933820eaf81a74a0bc7c25d1dc46f59e82dc6
carmelc <carmelc@wix.com>
2025-07-02 19:14:02 +0300

    run tests on deploy preview

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.WIX_DEPLOY_URL || 'http://localhost:4321',
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:4321',
     
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -39,7 +39,7 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests - only when not using deployed URL */
-  ...(process.env.WIX_DEPLOY_URL ? {} : {
+  ...(process.env.PLAYWRIGHT_TEST_BASE_URL ? {} : {
     webServer: {
       command: 'npm run dev',
       url: 'http://localhost:4321',
