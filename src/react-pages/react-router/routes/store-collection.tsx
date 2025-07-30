dc54df7d6b5049ec142fcd89198c9416cce5e54b
Yury Michurin <yury.michurin@gmail.com>
2025-07-30 09:52:43 +0300

    Bring back skeleton to collection page (#110)
    * Bring back skeleton to collection page
    
    * Move the comment to proper place

--- src/react-pages/react-router/routes/store-collection.tsx
+++ src/react-pages/react-router/routes/store-collection.tsx
@@ -80,7 +80,6 @@ export async function storeCollectionRouteLoader({
     throw new Response('Not Found', { status: 404 });
   }
 
-  // Start collection data load but don't await it (deferred)
   const searchOptions = {
     cursorPaging: {
       limit: 20,
@@ -92,15 +91,16 @@ export async function storeCollectionRouteLoader({
     },
   };
 
-  const [productsListConfig, productsListFiltersConfig] = await Promise.all([
+  // Start collection data load but don't await it,
+  // It will be awaited in the route component for the skeleton to be rendered
+  const notAwaiatedData = Promise.all([
     loadProductsListServiceConfig(searchOptions),
     loadProductsListFiltersServiceConfig(),
   ]);
 
   return {
     categoriesConfig,
-    productsListConfig,
-    productsListFiltersConfig,
+    notAwaiatedData,
     currentCategorySlug: params.categorySlug,
   };
 }
@@ -113,26 +113,27 @@ export function StoreCollectionRoute({
   storeRoute: string;
 }) {
   const basename = useHref('/');
-  const {
-    categoriesConfig,
-    productsListConfig,
-    productsListFiltersConfig,
-    currentCategorySlug,
-  } = useLoaderData<typeof storeCollectionRouteLoader>();
+  const { categoriesConfig, notAwaiatedData, currentCategorySlug } =
+    useLoaderData<typeof storeCollectionRouteLoader>();
 
   return (
     <div className="wix-verticals-container">
       {/* Collection/products load with skeleton using React Router's Await */}
       <React.Suspense fallback={<CollectionSkeleton />}>
-        <Await resolve={productsListConfig} errorElement={<CollectionError />}>
-          <CategoryPage
-            categoriesListConfig={categoriesConfig}
-            productsListConfig={productsListConfig}
-            productsListFiltersConfig={productsListFiltersConfig}
-            currentCategorySlug={currentCategorySlug}
-            productPageRoute={productPageRoute}
-            basePath={`${basename}${storeRoute}`}
-          />
+        <Await resolve={notAwaiatedData} errorElement={<CollectionError />}>
+          {data => {
+            const [productsListConfig, productsListFiltersConfig] = data;
+            return (
+              <CategoryPage
+                categoriesListConfig={categoriesConfig}
+                productsListConfig={productsListConfig}
+                productsListFiltersConfig={productsListFiltersConfig}
+                currentCategorySlug={currentCategorySlug}
+                productPageRoute={productPageRoute}
+                basePath={`${basename}${storeRoute}`}
+              />
+            );
+          }}
         </Await>
       </React.Suspense>
     </div>
