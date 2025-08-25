b6a54ac158a7f1f78641cd8ba7619c68d50f449c
nitayneeman <7141972+nitayneeman@users.noreply.github.com>
2025-08-25 08:31:48 +0300

    implement

--- src/components/store/ui/ProductList.tsx
+++ src/components/store/ui/ProductList.tsx
@@ -101,10 +101,10 @@ export const LoadMoreTrigger = React.forwardRef<
                     d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                   ></path>
                 </svg>
-                {props.labels.loading}
+                {props.labels.loading || 'Loading...'}
               </span>
             ) : (
-              props.labels.hasMoreProducts
+              props.labels.hasMoreProducts || 'Load More Products'
             )}
           </Button>
         </ProductListPrimitive.LoadMoreTrigger>
