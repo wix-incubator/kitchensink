80d5d50e3409f3cfb5e302973e57ab03317d6d55
arielWix <arielh@wix.com>
2025-08-25 15:31:02 +0300

    more places

--- src/react-pages/cart.tsx
+++ src/react-pages/cart.tsx
@@ -1,7 +1,7 @@
 import '../styles/theme-1.css';
 import { KitchensinkLayout } from '../layouts/KitchensinkLayout';
 import CartContent from '../components/ecom/Cart';
-import { CurrentCart } from '@wix/headless-ecom/react';
+import { CurrentCart } from '@/components/ui/ecom/CurrentCart';
 import type { CurrentCartServiceConfig } from '@wix/headless-ecom/services';
 
 interface CartPageProps {
@@ -11,14 +11,14 @@ interface CartPageProps {
 export default function CartPage({ currentCartServiceConfig }: CartPageProps) {
   return (
     <KitchensinkLayout>
-      <CurrentCart.Root currentCartServiceConfig={currentCartServiceConfig}>
+      <CurrentCart currentCartServiceConfig={currentCartServiceConfig}>
         <div className="container mx-auto px-4 py-8">
           <h1 className="text-content-primary text-4xl font-bold mb-4">
             Shopping Cart
           </h1>
           <CartContent />
         </div>
-      </CurrentCart.Root>
+      </CurrentCart>
     </KitchensinkLayout>
   );
 }
